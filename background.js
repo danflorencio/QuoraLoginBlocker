a1148e3bfc378303bb4923078bcbc7c66243c03f
Daniel Florencio <danflorencio94@gmail.com>
2017-03-14 03:36:11 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -16,12 +16,15 @@
       var url = new URL(tab.url);
       var domain = url.hostname;
       console.log(url.href.charAt(url.href.length-8));
-      if (domain === "www.quora.com") {
+      if (domain === "www.quora.com" && !(url.href.includes("https://www.quora.com/search?"))
+          && !(url.href.includes("https://www.quora.com/sitemap"))
+          && !(url.href.includes("https://www.quora.com/topic"))) {
         console.log("let's block it")
         if (url.href.charAt(url.href.length-8) != "?") {
           url += "?share=1";
           redirect = url;
           reload();
+          console.log("blocked");
         }
         console.log(url);
         return;
